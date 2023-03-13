d0d8ae14e0f657d392121d46c389f700349de4bc
Enak1n <masj963@gmail.com>
2023-03-13 21:01:37 +0300

    initial commit

---
+++ HotelBookingAPI/HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            :base (options)
+        {
+
+        }
+    }
+}
